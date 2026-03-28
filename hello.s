ba30c50c898bdd933a1cbb38fc8c627739b64c63
MMazenA <jazenjirza@gmail.com>
2026-03-28 15:02:06 -0400

    progress

--- hello.s
+++ hello.s
@@ -7,8 +7,14 @@
     .section .text.enter
     .global _start
 
+
+
+// Stack pointer register - sp
+// li instruction loads instruction
+// li is considered an integer computation because it loads an integer in my stack pointer
+
 _start:
     li sp, 0x80400000
-    call main
+    call main // this is considered a control transfer instruction, we transfer control to hello.c::main
 _end:
     call _end
